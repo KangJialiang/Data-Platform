bb3a9cf1521d83ef30b1afe734f69c241a81ca3f
KangJialiang <60818198+KangJialiang@users.noreply.github.com>
2022-01-12 21:53:44 -0800

    fix: remove unused return value in main function

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 
 int main(int argc, char* argv[]) {
   QApplication a(argc, argv);
-  FILE* filePipe = popen("roscore&", "r");  // start roscore in background
+  popen("roscore&", "r");  // start roscore in background
   ros::init(argc, argv, "node");
   MainWindow w;
   w.show();
